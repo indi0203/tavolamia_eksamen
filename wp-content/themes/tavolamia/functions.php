f481e5a320e03b8f2aadd83eeadef13f105655a5
“indi0203” <indi0203@stud.kea.dk>
2022-06-01 20:03:50 +0200

    styling

--- wp-content/themes/tavolamia/functions.php
+++ wp-content/themes/tavolamia/functions.php
@@ -21,4 +21,35 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
+
 // END ENQUEUE PARENT ACTION
+
+function my_scripts_method() {
+    wp_enqueue_script(
+          'custom-script',
+          get_stylesheet_directory_uri() . '/js/topbutton.js',
+          array( 'jquery' )
+    );
+}
+
+// tilføjer description under thumbnail
+
+add_action( 'woocommerce_before_shop_loop_item_title', 'wc_add_long_description' );
+function wc_add_long_description() {
+	global $product;
+
+	?>
+        <div class="øverest" itemprop="description">
+            <?php echo apply_filters( 'the_content', $product->get_description() ) ?>
+        </div>
+	<?php
+}
+
+
+// tilføjer divider billede på single product side
+
+add_action('woocommerce_after_single_product_summary','mycustomfuncion',11);
+function mycustomfuncion()
+{
+    echo '<div class="my_divider"><img src="http://indiamillward.dk/kea/tavolamia/wp-content/uploads/2022/05/divider-13.png" alt="" /></div>';
+}
